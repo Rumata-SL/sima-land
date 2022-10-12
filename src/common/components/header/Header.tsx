33db1bf3ff15d6765b733fa497558c5f56bbd894
Rumata-SL <kirr7619@gmail.com>
2022-10-13 04:44:06 +0500

    Added Logo.tsx, changed components App.css, AppLayout.tsx, Header.module.css, Header.tsx, Login.tsx, Login.module.css

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { Logo } from '../logo/Logo'
+
 import style from './Header.module.css'
 
 export const Header = () => {
-  return <div className={style.container}>Header</div>
+  return (
+    <div className={style.container}>
+      <div className={style.svgBlock}>
+        <Logo />
+      </div>
+    </div>
+  )
 }
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import '../app/App.css'
+
 import { Outlet } from 'react-router-dom'
 
 import { Header } from '../common/components/header/Header'
 
 export const AppLayout = () => {
   return (
-    <div className="app">
+    <div className="App">
       <Header />
       <div className="wrapper">
         <Outlet />
