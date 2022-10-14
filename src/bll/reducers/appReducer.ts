ccd75ba3828350d4602a9d017a2af452ebb528c0
Rumata-SL <kirr7619@gmail.com>
2022-10-14 20:06:23 +0500

    added appReducer.ts, rootReduser.ts, wrote the logic

---
+++ src/bll/reducers/appReducer.ts
@@ -0,0 +1,88 @@
+import { ThunkType } from '../types/types'
+
+type InitialStateType = typeof initialState
+
+const initialState = {
+  login: '',
+  email: '',
+  country: '',
+  city: '',
+  isInitialized: false,
+  isLoggedIn: false,
+  isAddress: false,
+  isVerified: false,
+}
+
+export const appReducer = (
+  state: InitialStateType = initialState,
+  action: AppActionType
+): InitialStateType => {
+  switch (action.type) {
+    case 'app/SET-IS-INITIALIZED':
+      return { ...state, isInitialized: action.value }
+    case 'app/SET-LOGIN':
+      return { ...state, login: action.login, email: action.email, isLoggedIn: action.isLoggedIn }
+    case 'app/SET-ADDRESS':
+      return { ...state, country: action.country, city: action.city, isAddress: action.isAddress }
+    case 'app/SET-IS-VERIFIED':
+      return { ...state, isInitialized: action.isInitialized, isVerified: action.isVerified }
+    default:
+      return state
+  }
+}
+
+//AC
+export const setAppInitialized = (value: boolean) => {
+  return {
+    type: 'app/SET-IS-INITIALIZED',
+    value,
+  } as const
+}
+export const setLogin = (login: string, email: string, isLoggedIn: boolean) => {
+  return {
+    type: 'app/SET-LOGIN',
+    login,
+    email,
+    isLoggedIn,
+  } as const
+}
+
+export const setAddress = (country: string, city: string, isAddress: boolean) => {
+  return {
+    type: 'app/SET-ADDRESS',
+    country,
+    city,
+    isAddress,
+  } as const
+}
+export const setIsVerified = (isInitialized: boolean, isVerified: boolean) => {
+  return {
+    type: 'app/SET-IS-VERIFIED',
+    isInitialized,
+    isVerified,
+  } as const
+}
+
+// Thunks
+export const loginTC =
+  (login: string, email: string, isLoggedIn: boolean): ThunkType =>
+  dispatch => {
+    dispatch(setLogin(login, email, isLoggedIn))
+  }
+export const addressTC =
+  (country: string, city: string, isAddress: boolean): ThunkType =>
+  dispatch => {
+    dispatch(setAddress(country, city, isAddress))
+  }
+export const verifiedTC =
+  (isInitialized: boolean, isVerified: boolean): ThunkType =>
+  dispatch => {
+    dispatch(setIsVerified(isInitialized, isVerified))
+  }
+
+// Action Types
+export type AppActionType =
+  | ReturnType<typeof setAppInitialized>
+  | ReturnType<typeof setLogin>
+  | ReturnType<typeof setAddress>
+  | ReturnType<typeof setIsVerified>
